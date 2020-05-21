75d2377cbc714d95305b7089d105ebf143cf69ca
jetsonhacks <jim@jetsonhacks.com>
2020-05-21 08:10:14 -0700

    Less verbose during copy

--- copy-rootfs-ssd.sh
+++ copy-rootfs-ssd.sh
@@ -2,5 +2,6 @@
 # Mount the SSD as /mnt
 sudo mount /dev/nvme0n1p1 /mnt
 # Copy over the rootfs from the SD card to the SSD
-sudo rsync -aAXv --numeric-ids --info=progress2 --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/*","/lost+found"} / /mnt
-# Keep the SSD mounted for further operations
+sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/*","/lost+found"} / /mnt
+# We want to keep the SSD mounted for further operations
+# So we do not unmount the SSD
