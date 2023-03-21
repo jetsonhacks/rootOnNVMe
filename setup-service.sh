fe2c4c8e24e3d5b4a9293a60c91a92fbceaa3294
Linir Zamir <alzcorp@gmail.com>
2023-03-21 14:31:20 -0400

    change setup service

--- setup-service.sh
+++ setup-service.sh
@@ -15,6 +15,8 @@ fi
 # Setup the service to set the rootfs to point to the SSD
 sudo cp data/setssdroot.service /etc/systemd/system
 sudo cp data/setssdroot.sh /sbin
+sudo bash -c "echo 'DEVICE_TYPE=$device_type' > /etc/setssdroot.conf"
+
 sudo chmod 777 /sbin/setssdroot.sh
 systemctl daemon-reload
 sudo systemctl enable setssdroot.service
@@ -26,7 +28,6 @@ sudo cp /sbin/setssdroot.sh /mnt/sbin/setssdroot.sh
 # Create setssdroot.conf which tells the service script to set the rootfs to the SSD
 # If you want to boot from SD again, remove the file /etc/setssdroot.conf from the SD card.
 # touch creates an empty file
-sudo bash -c "echo 'DEVICE_TYPE=$device_type' > /etc/setssdroot.conf"
 echo 'Service to set the rootfs to the SSD installed.'
 echo 'Make sure that you have copied the rootfs to SSD.'
 echo 'Reboot for changes to take effect.'
